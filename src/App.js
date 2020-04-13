158ac56991fb87d0cd42b7b58fa6f1c93fb402a7
chamone8 <chamone8@gmail.com>
2020-04-13 09:53:16 -0300

    Projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
-import React from 'react';
-import './slide';
-import './style.css';
-import portifolio1 from './temp/2.jpg';
-import portifolio2 from './temp/3.jpg'
-import portifolio3 from './temp/4.jpg'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+import App from './Paginas/principal';
+import Curriculo from './Paginas/Curriculo';
+
+
+export default function BasicExample() {
   return (
-    <main>
-      <header>
-        <nav>
-        <div className="slider" >
-          <img className="minhaImagem" src='https://source.unsplash.com/random/100x100' alt='Foto de Fundo' />
-          </div>
-          <h3 className="nome">Felipe Chamone   </h3>
-          <ul>
-            <li>Curriculo</li>
-            <li>Enconte-me</li>
-            <li>Portifolio</li>
-            <ul>
-              <li>
-                Box
-              </li>
-              <li>
-                Um outro
-              </li>
-              <li>
-                Hero
-              </li>
-            </ul>
-          </ul>
-        </nav>
-      </header>
-      <section>
-        <div className="container">
-          <div className="minhasInformacoes">
-            <h5>Algumas informações</h5>
-            <p className="">Era spara parecer um home office Era para parecer um home officeEra para parecer um home officeEra para parecer um home officeEra para parecer um home officeEra para parecer um home officeEra para parecer um home officeEra para parecer um home officeEra para parecer um home office</p>
-          </div>
-
-          <div className="portifolio" data-js="sslide">
-             
-             <img className="portifolioImg" data-js="sslide" src={portifolio1} alt="portifolio"/>
-             <img className="portifolioImg" data-js="sslide" src={portifolio2} alt="portifolio"/>
-             <img className="portifolioImg" data-js="sslide" src={portifolio3} alt="portifolio"/>
-          </div>
-
-        </div>
-      </section>
-    </main>
+    <Router>
+      
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/curriculo" component={Curriculo} />
+        </Switch>
+      
+    </Router>
   );
 }
-
-export default App;
---
+++ src/Paginas/principal/index.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import './slide';
+import './style.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// import AwesomeSlider from 'react-awesome-slider';
+// import 'react-awesome-slider/dist/styles.css';
+
+import portifolio1 from '../.././temp/2.jpg';
+
+
+function App() {
+  function mostrarData(){
+    let date = new Date();
+    let ano = date.getUTCFullYear();
+    let mes = date.getMonth();
+    let meses = "";
+    let dia = date.getDate();
+    
+    if(mes == 3){
+       meses = "Mar"
+    }else if(mes == 4){
+       meses = "Abr"
+    }
+    
+    return dia + " de " + meses + " " + ano + " Belo Horizonte";
+
+  }
+  return (
+    <main>
+      <header>
+        <nav>
+          <div className="slider" >
+            <img className="minhaImagem" src='https://source.unsplash.com/random/100x100' alt='Foto de Fundo' />
+          </div>
+          <h3 className="nome">Felipe Chamone   </h3>
+          <ul>
+
+            <li className="list1">
+              <Link className="list" to="/curriculo" >Curriculo</Link>
+            </li>
+            <li className="list1">
+              <Link className="list" to="/Encotre-me">Enconte-me</Link>
+            </li>
+            <div className="dropdown ">
+              <li className="list1">Portifolio</li>
+              <div className="dropdown-content">
+                <ul>
+                  <li>
+                    <a href="https://frontbox.herokuapp.com/">ChamoneBox</a>
+                  </li>
+                  <li>
+                    <a href="https://cadastrodev.herokuapp.com/">Cadastro Dev</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </ul>
+        </nav>
+      </header>
+      <section>
+        <div className="container">
+          <div className="minhasInformacoes">
+            <h5>Algumas informações</h5>
+
+
+            <p className="">Meu nome é Felipe Chamone, tenho 28 anos Estudo na faculdade Pitágoras,
+             sou apaixonado com tecnologia e amo desenvolvimento, tenho como foco entrar como 
+             desenvolvedor Web no mercado de trabalho. Tenho experiência com desenvolvimento em 
+             front end </p>
+
+
+          </div>
+
+          <div className="portifolio" data-js="sslide">
+
+            <img className="portifolioImg" data-js="sslide" src={portifolio1} alt="portifolio" />
+
+          </div>
+
+        </div>
+      </section>
+
+      <footer>
+        <div class="footer">
+          { mostrarData()  }
+
+        </div>
+      </footer>
+    </main>
+  );
+}
+
+export default App;
